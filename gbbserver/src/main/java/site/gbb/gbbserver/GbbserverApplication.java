d938395a6b7408eeeb200735100a8a881a873584
seokyeong <purewater71@naver.com>
2023-11-08 23:01:08 +0900

    [CONFIG] initial setting

---
+++ gbbserver/src/main/java/site/gbb/gbbserver/GbbserverApplication.java
@@ -0,0 +1,13 @@
+package site.gbb.gbbserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbbserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbbserverApplication.class, args);
+	}
+
+}

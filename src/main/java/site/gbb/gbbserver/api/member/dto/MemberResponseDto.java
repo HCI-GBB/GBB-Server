c02dce8c645a3c6a52c08d5c876de9b5e3db608f
hancrysta1 <hancrystal@sookmyung.ac.kr>
2023-11-23 22:49:03 +0900

    [ADD] GET memberID-중간결과

--- src/main/java/site/gbb/gbbserver/api/member/dto/MemberResponseDto.java
+++ src/main/java/site/gbb/gbbserver/api/member/dto/MemberResponseDto.java
@@ -1,17 +1,27 @@
 package site.gbb.gbbserver.api.member.dto;
 
 import lombok.Getter;
+import site.gbb.gbbserver.api.hobby.domain.Hobby;
 import site.gbb.gbbserver.api.member.domain.Member;
+import site.gbb.gbbserver.api.result.domain.Result;
+
+import java.util.List;
 
 
 @Getter
 public class MemberResponseDto {
     private Long id;
     private String nickname;
+    private List<String> likes;//from Hobby
+    private List<String> hates;//from Hobby
+    private List<String> active;//from result
 
-    public MemberResponseDto(Member member){
+    public MemberResponseDto(Member member, Hobby hobby, Result result){
         this.id = member.getId();
         this.nickname = member.getNickname();
+        this.likes = hobby.getLikes();
+        this.hates = hobby.getHates();
+        this.active = result.getActive();
     }
 
 
--- src/main/java/site/gbb/gbbserver/api/member/service/MemberService.java
+++ src/main/java/site/gbb/gbbserver/api/member/service/MemberService.java
@@ -3,43 +3,38 @@ package site.gbb.gbbserver.api.member.service;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import site.gbb.gbbserver.api.hobby.repository.HobbyRepository;
 import site.gbb.gbbserver.api.member.domain.Member;
 import site.gbb.gbbserver.api.member.repository.MemberRepository;
 import site.gbb.gbbserver.api.member.dto.MemberRequestDto;
 import site.gbb.gbbserver.api.member.dto.MemberResponseDto;
+import site.gbb.gbbserver.api.result.repository.ResultRepository;
 
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final HobbyRepository hobbyRepository;
+    private final ResultRepository resultRepository;
+
 
 
     @Transactional
-    public String join(MemberRequestDto member) {
+    public Member join(MemberRequestDto member) {
         validateDuplicateMember(member.toEntity());
         save(member);
-        return memberRepository.findByNickname(member.getNickname()).get().getNickname();
-    }
-    @Transactional
-    public MemberResponseDto show(Long id) {
-        if(memberRepository.findById(id).isPresent()){
-            return findById(id);
-        }else
-            return validateMember(id);
+        return memberRepository.findByNickname(member.getNickname()).get();
     }
 
     public String save(MemberRequestDto memberRequestDto){
         return memberRepository.save(memberRequestDto.toEntity()).getNickname();
     }
+    @Transactional
     public MemberResponseDto findById(Long id){
-        Member member = memberRepository.findById(id).get();
-        return new MemberResponseDto(member);
-    }
-
-    private MemberResponseDto validateMember(Long id) {
-        Member m = memberRepository.findByIdOrThrow(id);
-        return new MemberResponseDto(m);
+        return new MemberResponseDto(memberRepository.findByIdOrThrow(id),
+                    hobbyRepository.findById(id).get(),
+                    resultRepository.findById(id).get());
     }
     private void validateDuplicateMember(Member member) {
         memberRepository.findByNickname(member.getNickname())

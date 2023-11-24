bab09acec3a9b8bd4b3cd43a0578373bb9aa0b97
Seokyeong <purewater71@naver.com>
2023-11-24 14:11:10 +0900

    [Fix] allowOrigin localhost만

--- src/main/java/site/gbb/gbbserver/config/WebMvcConfig.java
+++ src/main/java/site/gbb/gbbserver/config/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:3000", "http://localhost:3001")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
